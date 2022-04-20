a7a77ce57e4cdbbdc1d41a17101603fb6603eff3
Xubenbenn <51053533+Xubenbenn@users.noreply.github.com>
2022-04-20 13:37:53 +0100

    Update FallDetector.cpp

--- src/FallDetector.cpp
+++ src/FallDetector.cpp
@@ -4,7 +4,7 @@
 
 int counter = 0;
 
-
+//返回vec集合的标准差
 double FallDetector::getStddev(vector<double>* vec) {
     Scalar mean, stddev;
     if(!vec->empty()) {
@@ -13,12 +13,14 @@ double FallDetector::getStddev(vector<double>* vec) {
     return stddev[0];
 }
 
+//根据当前帧和历史帧，从中提取出人的运动系数
 double FallDetector::getMovementCoefficient(Mat* foreground,Mat* history) {
     double sumForeground = sum(*foreground)[0];
     double sumHistory = sum(*history)[0];
     return (sumHistory / sumForeground) * 100.0;
 }
 
+//判断人摔倒后是否不再运动，若是则判断成功，没有起身
 void FallDetector::checkIfStaysInPlace(time_t start, bool* isChecking, bool* isFall, vector<double> xPos, vector<double> yPos) {
     double secondsSinceStart = difftime( time(0), start);
     double xDevValue;
@@ -27,7 +29,7 @@ void FallDetector::checkIfStaysInPlace(time_t start, bool* isChecking, bool* isF
 
     xDevValue = this->getStddev(&xPos);
     yDevValue = this->getStddev(&yPos);
-
+    //获取x,y方向点集的标准差，标准差小则意味着位置保持稳定
 
     if(xDevValue < 2 && yDevValue < 2) {
         *isFall = true;
@@ -37,22 +39,24 @@ void FallDetector::checkIfStaysInPlace(time_t start, bool* isChecking, bool* isF
         counter = 0;
         }
         
+        //连续6帧保持稳定则确认跌倒
 
     }
 
     if (!isFall && secondsSinceStart > 2) {
         *isChecking = false;
+        //帧与帧之间间隔大说明人静止的状态不连续，因此人起身了
     }
 }
 
 void FallDetector::analyzePosition(Mat* frame, vector<double>* thetaRatio, vector<double>* aRatio, vector<double>* bRatio, vector<double>* xPos, vector<double>* yPos, vector<Point> largestContour) {
-    Rect boundingRectangle = boundingRect(largestContour);
-    rectangle(*frame, boundingRectangle, Scalar(0, 255, 0), 2);
+    Rect boundingRectangle = boundingRect(largestContour);//确定矩形的形状
+    rectangle(*frame, boundingRectangle, Scalar(0, 255, 0), 2);//在图中画出矩形
 
     if(largestContour.size() > 5) {
 
-        RotatedRect e = fitEllipse(largestContour);
-        ellipse( *frame, e, Scalar(255, 0, 0), 2 );
+        RotatedRect e = fitEllipse(largestContour);// 确定椭圆的形状
+        ellipse( *frame, e, Scalar(255, 0, 0), 2 );// 在图中画出椭圆
 
         thetaRatio->push_back(e.angle);
 
@@ -65,7 +69,9 @@ void FallDetector::analyzePosition(Mat* frame, vector<double>* thetaRatio, vecto
         double y = e.center.y;
         xPos->push_back(x);
         yPos->push_back(y);
-
+        //从帧frame以及人的框架点集largestContour中提取出椭圆相关信息，长轴短轴中心点位置等等. 存到入参的vector中。
+        
+        //最多存放10帧的信息，超出则从vector头部开始删除。
         if(thetaRatio->size() > 10) {
             thetaRatio->erase(thetaRatio->begin());
         }
@@ -86,6 +92,7 @@ void FallDetector::analyzePosition(Mat* frame, vector<double>* thetaRatio, vecto
 }
 
 void FallDetector::checkMovementAfterFall(bool* toBeChecked, bool *isFall, vector<double> xPos, vector<double> yPos) {
+    //人跌倒判断后续 tobechecked标志位的复位。
     double xDevValue = this->getStddev(&xPos);
     double yDevValue = this->getStddev(&yPos);
 
